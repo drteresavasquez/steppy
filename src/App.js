036158f2fc5c3f7221fbfc53d21074bdaba9348d
Teresa Vasquez <tvasquez@tvasquez.local>
2019-01-19 15:21:17 -0600

    updated data set

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import data from './data.json'
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
+    const lessons = data.lessons.map((lesson, i)=>{
+      return(
+      <LessonTemplate
+        key={i}
+        title={lesson.title}
+        color1={lesson.color1}
+        color2={lesson.color2}
+      />
+      )
+    })
+
+    console.log(data.lessons);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {lessons}
       </div>
     );
+  }
+}
+
+class LessonTemplate extends React.Component {
+  render(){
+    return(
+      <>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.color1}</p>
+        <p>{this.props.color2}</p>
+        <Button bsStyle="primary">Primary</Button>
+      </>
+    )
   }
 }
 
