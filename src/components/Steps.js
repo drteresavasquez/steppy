0276b541d24f8e7432526ccc12b012e89b58d158
Teresa Vasquez <tvasquez@tvasquez.local>
2019-01-19 22:36:51 -0600

    updated steps

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import { Button, Image, Modal } from 'semantic-ui-react'
 import './Steps.css';
 export default class Steps extends Component {
     state={
