60663cc16234d944f2c321d4d7c2467925944396
Teresa Vasquez <tvasquez@tvasquez.local>
2019-01-19 22:02:15 -0600

    updated steps to use next button

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,55 +1,43 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 import './Steps.css';
 export default class Steps extends Component {
     state={
         steps: this.props.data.steps,
         title: this.props.data.title,
-        currentStep:1
+        bgColor: this.props.data.color1,
+        currentStep:0
     }
 
-    render(){
-        const frames = this.state.steps.map((step, i)=>{
+    nextLesson = (num)=>{
+        this.setState({
+            currentStep:num+1
+          })
+    }
+        render(){
+            const imgUrl = '../img/screen_plain.png';
+            const divStyle = { 
+                backgroundColor: `${this.state.bgColor}`
+            };
+
             return(
-                <Frame 
-                    key={i}
-                    step={i+1}
-                    title={this.state.title}
-                    step_description={step.step_description}
-                    step_img={step.step_img}
-                />
-    
-            )
-        })
-        console.log(this.state.steps[1]);
-        return(
-            <>
-            {frames}
-            </>
-        )
-    }
-}
-
-class Frame extends Component {
-    render(){
-
-        var imgUrl = '../img/screen_plain.png';
-        var divStyle = { 
-            background: '#eeeeff', 
-
-           
-         };
-
-
-        return(
             <div style={divStyle}>
             <div className="frame-container">
-                <h1>Step {this.props.step}: {this.props.title}</h1>
+                <h1>Step {this.state.currentStep + 1}: {this.state.title}</h1>
                 <div className="flex-container">
-                    <p>{this.props.step_description}</p>
-                    <img src={this.props.step_img} alt="this.props.step_description"/>
+                    <p>{this.state.steps[this.state.currentStep].step_description}</p>
+                    <img src={this.state.steps[this.state.currentStep].step_img} alt="this.props.step_description"/>
                 </div>
+                <Button 
+                    content="Next Step"
+                    size='massive'
+                    primary
+                    onClick={()=>{this.nextLesson(this.state.currentStep)}}
+                />
             </div>
-            </div>
+        </div>
         )
-    }
+
+   
+    } 
 }
